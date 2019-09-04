f7d51a13f0910aa272555ed272f3444ccfd9e197
Derek Miller <derekmlmt@yahoo.com>
2019-09-04 11:03:41 -0700

    added search button on-click and routing.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,22 @@
 var db = require("../models");
 
 module.exports = function(app) {
+
   // Get all bites
   app.get("/api/bites", function(req, res) {
     db.Bites.findAll({}).then(function(dbBites) {
+      res.json(dbBites);
+    });
+  });
+
+  //  Search Bites by Title
+  app.get("/api/bites/:title", function(req, res) {
+    var title = req.params.title;
+    db.Bite.findAll({
+      where: {
+        title: title,
+      }
+    }).then(function(dbBites) {
       res.json(dbBites);
     });
   });
@@ -15,24 +28,4 @@ module.exports = function(app) {
     });
   });
 
-  // Editing a Bite
-
-  app.put("/api/bites/:id", function(req, res) {
-    var body = req.body;
-
-    db.Bites.update(body, {
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbBite) {
-      res.json(dbBite);
-    })
-  })
-
-  // Delete a Bite by id
-  app.delete("/api/bites/:id", function(req, res) {
-    db.Bites.destroy({ where: { id: req.params.id } }).then(function(dbBite) {
-      res.json(dbBite);
-    });
-  });
 };
