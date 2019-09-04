7cf250e3a0e4b1601a950cf970937331b8b82b8a
Mick Arias <mick756@gmail.com>
2019-09-04 11:11:54 -0700

    ignore

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,22 @@
 var db = require("../models");
 
 module.exports = function(app) {
+
   // Get all bites
   app.get("/api/bites", function(req, res) {
     db.Bite.findAll({}).then(function(dbBites) {
+      res.json(dbBites);
+    });
+  });
+
+  //  Search Bites by Title
+  app.get("/api/bites/:title", function(req, res) {
+    var title = req.params.title;
+    db.Bite.findAll({
+      where: {
+        title: title,
+      }
+    }).then(function(dbBites) {
       res.json(dbBites);
     });
   });
@@ -12,32 +25,6 @@ module.exports = function(app) {
   app.post("/api/bites", function(req, res) {
     db.Bite.create(req.body).then(function(dbBites) {
       res.json(dbBites);
-    });
-  });
-
-  app.get("/user/:id", function (req, res) {
-    db.User.findAll({ where: { id: req.params.id } }).then(function(user) {
-      res.json(user.name);
-    });
-  });
-
-  // Editing a Bite
-  app.put("/api/bites/:id", function(req, res) {
-    var body = req.body;
-
-    db.Bite.update(body, {
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbBite) {
-      res.json(dbBite);
-    })
-  });
-
-  // Delete a Bite by id
-  app.delete("/api/bites/:id", function(req, res) {
-    db.Bite.destroy({ where: { id: req.params.id } }).then(function(dbBite) {
-      res.json(dbBite);
     });
   });
 };
